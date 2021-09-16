d339bbf7bdaeeb14e19a807fe942b9600651a6af
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-16 16:34:57 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,9 @@
+# wapp to convert amt in $ and convert to rs
+
+amount = float(input("enter amount in $ "))
+rupees = amount * 74.56
+
+print("amount in \u0024", amount)
+print("amount in \u20B9", rupees)
+print("amount in \u20B9", round(rupees,2))
+print("amount in \u20B9", round(rupees,1))
